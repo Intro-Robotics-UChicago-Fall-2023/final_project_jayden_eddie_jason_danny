32ffd1650424826533bc06bd27af9d0e6059716d
Jayden Cole Francis <jayden@csil5-4.cs.uchicago.edu>
2023-11-13 21:05:06 -0600

    added inverse kinematics stuff

--- scripts/move_to_can.py
+++ scripts/move_to_can.py
@@ -30,6 +30,8 @@ class FindAndPickupCan:
                 self.arm_len_1 = 0.2
                 self.arm_len_2 = 0.274
                 self.can_to_pick = 'sprite'
+
+                self.latest_image = None
                 self.new_image_flag = False
         
         def laser_callback(self, data):
@@ -52,10 +54,7 @@ class FindAndPickupCan:
                 # calculate the minimum distance in that range, ignoring 'inf' values
                 self.distance_in_front = min(r for r in front_ranges if r != float('inf'))
                 print(self.distance_in_front)
-                
-        
-
-            
+                   
         
         def get_lidar_distance(self):
             return self.distance_in_front
@@ -80,10 +79,27 @@ class FindAndPickupCan:
             self.latest_image = self.image
             self.new_image_flag = True
         
-        def calculate_angles(self, x, y):
-            q2 = math.acos((x ** 2 + y ** 2 - self.arm_len_1**2 - self.arm_len_2**2) / (2*self.arm_len_1*self.arm_len_2))
-            q1 = math.atan(y/x) - math.atan((self.arm_len_2*math.sin(q2))/(self.arm_len_1 + self.arm_len_2*math.cos(q2)))
+        def calculate_angles(self, x, z):
+           
+            r = math.sqrt(x**2 + z**2)
+
+            
+            cos_q2 = (r ** 2 - self.arm_len_1**2 - self.arm_len_2**2) / (2 * self.arm_len_1 * self.arm_len_2)
+            q2 = math.acos(cos_q2)
+
+        
+            q1 = math.atan2(z, x) - math.atan2(self.arm_len_2 * math.sin(q2), self.arm_len_1 + self.arm_len_2 * math.cos(q2))
+
             return q1, q2
+
+        
+        def calculate_z(self, cx, cy, lidar_distance):
+            image_height = self.image.shape[0]
+            camera_vertical_fov = math.radians(48.8) 
+            angle_from_center = ((cy - (image_height / 2)) / (image_height / 2)) * (camera_vertical_fov / 2)
+            z = self.distance_in_front * math.tan(angle_from_center)
+
+            return z
 
         
         def detect_can(self, image, can):
@@ -94,18 +110,84 @@ class FindAndPickupCan:
                     'sprite': (np.array([55, 40, 180]), np.array([125, 120, 230]))
                 }
             target_center = None
-            self.object_found = False
+            
             if color in color_ranges:
                     lower_color, upper_color = color_ranges[color]
                     mask = cv2.inRange(hsv, lower_color, upper_color)
                     M = cv2.moments(mask)
                     if M['m00'] > 0:
-                        cx = int(M['m10']/M['m00'])
-                        cy = int(M['m01']/M['m00'])
+                        self.cx = int(M['m10']/M['m00'])
+                        self.cy = int(M['m01']/M['m00'])
                         target_center = (cx, cy)
                         cv2.circle(self.image, (cx, cy), 20, (0,0,255), -1)
-                        self.object_found = True
+                        
             return target_center
+        
+        def rotate_and_find_object(self):
+            rate = rospy.Rate(10)  
+            while self.distance_in_front >= 0.25:
+                
+                if self.object_centered and self.distance_in_front <= 0.25:
+                    self.vel_pub.publish(Twist())
+                    rospy.loginfo("Object approached, stopping")
+                    break  # Exit the loop
+
+                # if we detect the object, center and move towards it
+                elif self.detected_object_center:
+                    err = self.detected_object_center[0] - self.image_width / 2
+                    angular_speed = -float(err) / 1000  
+
+                    # prop control
+                    if abs(err) > self.image_width * 0.05:  
+                        linear_speed = 0  
+                    else:
+                        
+                        linear_speed = 0.1
+
+                    
+                    vel_msg = Twist()
+                    vel_msg.angular.z = angular_speed
+                    vel_msg.linear.x = linear_speed
+                    self.vel_pub.publish(vel_msg)
+
+                else:
+                    # search for object
+                    vel_msg = Twist()
+                    vel_msg.angular.z = 0.1 
+                    self.vel_pub.publish(vel_msg)
+
+                rate.sleep()  
+
+            self.vel_pub.publish(Twist())
+            rospy.loginfo("Exiting rotate_and_find_object")
+
+        def pick_up(self):
+        # complete the pickup sequence
+            gripper_joint_open = [0.01, 0.01]
+            self.move_group_gripper.go(gripper_joint_open, wait=True)
+            rospy.sleep(1)
+            self.move_group_gripper.stop()
+            rospy.sleep(1)
+            x = 0.25
+            z = self.calculate_z(self.cx, self.cy, self.distance_in_front)
+            angle1, angle2 = self.calculate_angles(x, z)
+            pickup_angles = [angle1, angle2, 0, 0]
+            while not self.move_group_arm.go(pickup_angles, wait=True):
+                rospy.logerr("Pick up motion failed at init")
+                rospy.sleep(1)
+                
+            rospy.sleep(1)
+            self.move_group_arm.stop()
+              
+            gripper_joint_closed = [-0.01, -0.01]  
+            # Close the gripper to grasp the object
+            rospy.sleep(10)
+            if not self.move_group_gripper.go(gripper_joint_closed, wait=True):
+                rospy.logerr("Gripper close motion failed at pickup")
+                return False
+            rospy.sleep(1)
+            self.move_group_gripper.stop()
+            rospy.sleep(1)
         
         def run(self):
             
